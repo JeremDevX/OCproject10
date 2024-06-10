dc84d81d368ac65a96086f7d0f1debe47a53bece
Lavergnat Jérémie <lavergnatj@outlook.fr>
2024-06-10 10:50:04 +0200

    Delete one div useless div, add dependencies on useEffect and a clearTimeout

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -20,24 +20,24 @@ const Slider = () => {
     if (data) {
       nextCard();
     }
-  }, [data]);
+    return () => clearTimeout(nextCard);
+  }, [data, index]);
 
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <div key={event.title}>
-          <div
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
         </div>
